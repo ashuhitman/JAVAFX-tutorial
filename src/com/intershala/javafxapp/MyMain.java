ef9cdd571154ef511ee9135832b67bdabc3be23b
ashuhitman <ashu8306@gmail.com>
2018-08-19 04:24:18 +0530

    JavaFX app initial

---
+++ src/com/intershala/javafxapp/MyMain.java
@@ -0,0 +1,83 @@
+package com.intershala.javafxapp;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.util.Optional;
+
+public class MyMain extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("app_layout.fxml"));
+        VBox rootNode = loader.load();
+        MenuBar menuBar = createMenu();
+        rootNode.getChildren().addAll(menuBar);
+        Scene scene = new Scene(rootNode,500,350);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Hello JavaFX app");
+        primaryStage.show();
+
+    }
+    public MenuBar createMenu() {
+        //File Menu
+        Menu fileMenu = new Menu("File");
+        MenuItem newMenuItem = new MenuItem("New");
+
+        newMenuItem.setOnAction(event -> System.out.println("New Item clicked"));
+
+        SeparatorMenuItem separatorMenuItem = new SeparatorMenuItem();
+        MenuItem quitMneuItem = new MenuItem("Quit");
+
+        quitMneuItem.setOnAction(event -> {
+            Platform.exit();
+            System.exit(0);
+        });
+
+        fileMenu.getItems().addAll(newMenuItem,separatorMenuItem,quitMneuItem);
+        //Help Menu
+        Menu helpMenu = new Menu("Help");
+        MenuItem aboutMneuItem = new MenuItem("About");
+
+        aboutMneuItem.setOnAction(event -> {
+            aboutApp();
+        });
+
+        helpMenu.getItems().addAll(aboutMneuItem);
+        //Menu Bar
+        MenuBar menuBar = new MenuBar();
+        menuBar.getMenus().addAll(fileMenu, helpMenu);
+        return menuBar;
+    }
+
+    private void aboutApp() {
+        Alert alertDialog = new Alert(Alert.AlertType.INFORMATION);
+        alertDialog.setTitle("My First Desktop App");
+        alertDialog.setHeaderText("Learning JAVAFX");
+        alertDialog.setContentText("This is my first desktop app. I'm beginner but soon i will be pro");
+
+        ButtonType yesBtn = new ButtonType("Yes");
+        ButtonType noBtn = new ButtonType("No");
+        alertDialog.getButtonTypes().setAll(yesBtn,noBtn);
+
+        Optional<ButtonType> clickedBtn = alertDialog.showAndWait();
+
+        if (clickedBtn.isPresent() && clickedBtn.get() == yesBtn) {
+            System.out.println("yes btn was clicked");
+        }else {
+            System.out.println("no btn was clicked");
+        }
+    }
+
+    @Override
+    public void stop() throws Exception {
+        super.stop();
+        System.out.println("Stopped");
+    }
+}
